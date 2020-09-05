ee8b64196616c2968d5fd0ef255981b30d21f8f3
Sam Geen <samgeen@monet.(none)>
2020-09-06 06:42:00 +0900

    First commit

---
+++ build/temp.linux-x86_64-3.8/src/read_cells.o.d
@@ -0,0 +1,3 @@
+build/temp.linux-x86_64-3.8/src/read_cells.o: src/read_cells.c src/fio.h \
+ src/utils.h src/read_cells.h src/read_amr.h
+commandline: -I/home/samgeen/anaconda3/lib/python3.8/site-packages/numpy/core/include -I/home/samgeen/anaconda3/include/python3.8 -c-I/home/samgeen/anaconda3/lib/python3.8/site-packages/numpy/core/include -I/home/samgeen/anaconda3/include/python3.8
---
+++ pymses/__init__.py
@@ -0,0 +1,43 @@
+# License:
+#   Copyright (C) 2011 Thomas GUILLET, Damien CHAPON, Marc LABADENS. All Rights Reserved.
+#
+#   This file is part of PyMSES.
+#
+#   PyMSES is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   PyMSES is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PyMSES.  If not, see <http://www.gnu.org/licenses/>.
+# Convenient imports
+
+# Utils
+from . import utils
+
+# Sources and filters
+from .sources.ramses.output import RamsesOutput
+from . import sources
+from . import filters
+
+# Data transformation and analysis
+from . import core
+transformations = core.transformations
+from . import analysis
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+__all__ = ["RamsesOutput", "sources", "filters", "transformations", "analysis", "utils"]
+
+# Those following informations are automatically filled by ../update_version_number.py script 
+# from ../setup.py version number and ../.hg/tags.cache informations
+__version__  = '4.0.0'
+__revision__ = '$Revision: 967$'
+__date__     = '$Date: 2014-01-30 $'
+
+
