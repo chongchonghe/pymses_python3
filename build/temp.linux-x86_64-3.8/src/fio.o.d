16c5a54c707192744324440dcc2dadc5e122efa5
ChongChong He <che1234@umd.edu>
2024-04-12 01:03:29 -0400

    Made it working on zaratan; clean up

--- build/temp.linux-x86_64-3.8/src/fio.o.d
+++
@@ -1,2 +0,0 @@
-build/temp.linux-x86_64-3.8/src/fio.o: src/fio.c src/byteswap.h src/fio.h
-commandline: -I/home/stgeen0/anaconda3/lib/python3.8/site-packages/numpy/core/include -I/home/stgeen0/anaconda3/include/python3.8 -c-I/home/stgeen0/anaconda3/lib/python3.8/site-packages/numpy/core/include -I/home/stgeen0/anaconda3/include/python3.8
--- build/temp.linux-x86_64-3.8/src/read_amr.o.d
+++
@@ -1,3 +0,0 @@
-build/temp.linux-x86_64-3.8/src/read_amr.o: src/read_amr.c src/fio.h \
- src/utils.h src/read_amr.h
-commandline: -I/home/stgeen0/anaconda3/lib/python3.8/site-packages/numpy/core/include -I/home/stgeen0/anaconda3/include/python3.8 -c-I/home/stgeen0/anaconda3/lib/python3.8/site-packages/numpy/core/include -I/home/stgeen0/anaconda3/include/python3.8
